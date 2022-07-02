6a002f14def7a81525d3d5f3d1611a855b6383d7
Arvin mostafaei <70845998+arviinmo@users.noreply.github.com>
2022-07-03 01:11:29 +0430

    add Radio component

--- components/RadioOptions/RadioOptions.jsx
+++ components/RadioOptions/RadioOptions.jsx
@@ -2,15 +2,12 @@ import React from 'react'
 
 function RadioOptions() {
   return (
-    <div><div className="flex">
-    <div className="flex items-center h-5">
-        <input id="helper-radio" aria-describedby="helper-radio-text" type="radio" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+    <div>
+        <div className="flex items-center">
+            <input type="radio" value="" name="default-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+            <label htmlFor="default-radio-1" className="ml-2 text-lg font-medium text-gray-900 dark:text-gray-300">Default radio</label>
+        </div>
     </div>
-    <div className="ml-2 text-sm">
-        <label htmlFor="helper-radio" className="font-medium text-gray-900 dark:text-gray-300">Free shipping via Flowbite</label>
-        <p id="helper-radio-text" className="text-xs font-normal text-gray-500 dark:text-gray-300">For orders shipped from $25 in books or $29 in other categories</p>
-    </div>
-</div></div>
   )
 }
 
