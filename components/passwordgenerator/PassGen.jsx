333356a27bd077bc587173cb502e6b2bbb9b64c7
Arvin mostafaei <70845998+arviinmo@users.noreply.github.com>
2022-07-02 20:43:00 +0430

    added components

---
+++ components/passwordgenerator/PassGen.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PassGen() {
+  return (
+    <div>PassGen</div>
+  )
+}
+
+export default PassGen
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
+import PassGen from "../components/passwordgenerator/PassGen";
 
 export default function Home() {
   return (
@@ -13,7 +14,7 @@ export default function Home() {
       <div className="w-3/3 bg-slate-300 flex flex-col items-center justify-center h-screen">
         <div className="flex flex-row h-2/3 rounded-2xl place-items-center w-1/2 p-5 bg-blue-400">
           <div className="w-full h-full items-right bg-red-500">
-            
+            <PassGen />
           </div>
         </div>
         <footer className="bg-gray-100 mt-4 flex flex-wrap items-center">
