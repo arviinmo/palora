0492000e192008f80658ec73d48049002bece17e
Arvin mostafaei <70845998+arviinmo@users.noreply.github.com>
2022-07-03 01:02:49 +0430

    add Radio component

--- components/RadioOptions/RadioOptions.jsx
+++ components/RadioOptions/RadioOptions.jsx
@@ -1,20 +1,17 @@
 import React from 'react'
 
-function 
-() {
+function RadioOptions() {
   return (
-    <div>
-        <div class="flex">
-    <div class="flex items-center h-5">
-        <input id="helper-radio" aria-describedby="helper-radio-text" type="radio" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+    <div><div className="flex">
+    <div className="flex items-center h-5">
+        <input id="helper-radio" aria-describedby="helper-radio-text" type="radio" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
     </div>
-    <div class="ml-2 text-sm">
-        <label for="helper-radio" class="font-medium text-gray-900 dark:text-gray-300">Free shipping via Flowbite</label>
-        <p id="helper-radio-text" class="text-xs font-normal text-gray-500 dark:text-gray-300">For orders shipped from $25 in books or $29 in other categories</p>
+    <div className="ml-2 text-sm">
+        <label htmlFor="helper-radio" className="font-medium text-gray-900 dark:text-gray-300">Free shipping via Flowbite</label>
+        <p id="helper-radio-text" className="text-xs font-normal text-gray-500 dark:text-gray-300">For orders shipped from $25 in books or $29 in other categories</p>
     </div>
-    </div>
-    </div>
+</div></div>
   )
 }
 
-export default 
+export default RadioOptions
--- components/passwordgenerator/PassGen.jsx
+++ components/passwordgenerator/PassGen.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import PassLength from "../passLength/PassLength";
+import RadioOptions from "../RadioOptions/RadioOptions";
 
 function PassGen() {
   return (
     <div>
-      <div className="flex flex-row">
+      <div className="flex flex-row flex-wrap">
         <h1 className="text-2xl font-Yeseva">Password Length:</h1>
         <PassLength />
       </div>
+        <RadioOptions />
     </div>
   );
 }
