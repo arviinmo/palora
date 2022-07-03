2827564a614abc3f23605ec6e7719b6da22e499b
Arvin mostafaei <70845998+arviinmo@users.noreply.github.com>
2022-07-03 17:50:16 +0430

    edited some button style

--- components/passLength/PassLength.jsx
+++ components/passLength/PassLength.jsx
@@ -5,7 +5,7 @@ function PassLength() {
     <div>
       <select
         title="Select the length of your password."
-        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-fit ml-2 p-1.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block font-Poppins w-fit ml-2 p-1.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
       >
         <optgroup label="Weak"></optgroup>
         <option value="6">6</option>
