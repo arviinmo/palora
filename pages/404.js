cb25af5d9ca631a84878ed5d49905fcb96220ce8
Arvin mostafaei <70845998+arviinmo@users.noreply.github.com>
2022-07-04 23:57:50 +0430

    add 404.js page

---
+++ pages/404.js
@@ -0,0 +1,21 @@
+export default function Custom404() {
+  return (
+    <div className="flex flex-col items-center">
+      <h1 className="font-bold text-blue-600 text-9xl">404</h1>
+
+      <h6 className="mb-2 text-2xl font-bold text-center text-gray-800 md:text-3xl">
+        <span className="text-red-500">Oops!</span> Page not found
+      </h6>
+
+      <p className="mb-8 text-center text-gray-500 md:text-lg">
+        The page you’re looking for doesn’t exist.
+      </p>
+
+      <a
+        href="#"
+        className="px-6 py-2 text-sm font-semibold text-blue-800 bg-blue-100"
+        >Go home</a
+      >
+    </div>
+  )
+}
