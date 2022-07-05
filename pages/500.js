d82a88a2ea213e62b734b3a2af20c062ba894042
Arvin mostafaei <70845998+arviinmo@users.noreply.github.com>
2022-07-05 11:19:29 +0430

    testing my 500 error page

--- pages/500.js
+++ pages/500.js
@@ -4,7 +4,7 @@ export default function Custom500() {
       <h1 className="text-9xl font-extrabold text-black tracking-widest">
         500
       </h1>
-      <div className="bg-[#FF6A3D] text-white px-2 mt-6 text-xl rounded absolute">
+      <div className="bg-[#FF6A3D] font-Poppins text-white px-2 mt-6 text-xl rounded absolute">
         Server-side error occurred
       </div>
     </main>
