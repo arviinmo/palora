543a5efc187692859f39ad9face1376a630c510f
Arvin mostafaei <70845998+arviinmo@users.noreply.github.com>
2022-07-08 12:20:20 +0430

    add Charecter.js to cleanup my components

--- components/GenButton/GenButton.jsx
+++ components/GenButton/GenButton.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { numbers, upperCaseLetters, lowerCaseLetters, specialCharacters } from '../Charecter'
+
 
 function GenButton() {
   const handleGeneratePassword = () => {
--- components/RadioOptions/RadioOptions.jsx
+++ components/RadioOptions/RadioOptions.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useState } from "react";
-import { numbers, upperCaseLetters, lowerCaseLetters, specialCharacters } from './Character'
 
 
 function RadioOptions() {
