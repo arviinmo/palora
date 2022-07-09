e6cda4d19c1d794006a09ec22fbda90d429bff59
Arvin mostafaei <70845998+arviinmo@users.noreply.github.com>
2022-07-09 13:20:47 +0430

    add react toast!

--- components/GenButton/GenButton.jsx
+++ components/GenButton/GenButton.jsx
@@ -1,6 +1,12 @@
 import React from "react";
-import { numbers, upperCaseLetters, lowerCaseLetters, specialCharacters } from '../Charecter'
-
+import {
+  numbers,
+  upperCaseLetters,
+  lowerCaseLetters,
+  specialCharacters,
+} from "../Charecter";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function GenButton(props) {
   const handleGeneratePassword = () => {
@@ -32,46 +38,59 @@ function GenButton(props) {
   const notify = (message, hasError = false) => {
     if (hasError) {
       toast.error(message, {
-        position: "top-center",
-        autoClose: 5000,
+        position: "bottom-center",
+        autoClose: 2000,
         hideProgressBar: false,
         closeOnClick: true,
         pauseOnHover: true,
         draggable: true,
         progress: undefined,
       });
-    }
-    else {
+    } else {
       toast(message, {
         position: "top-center",
-        autoClose: 5000,
+        autoClose: 2000,
         hideProgressBar: false,
         closeOnClick: true,
         pauseOnHover: true,
         draggable: true,
         progress: undefined,
       });
-    }}
+    }
+  };
 
   const createPassword = (characterList) => {
-    let password = ""
-    const characterListLength = characterList.length
+    let password = "";
+    const characterListLength = characterList.length;
     for (let i = 0; i < props.passwordLength; i++) {
-      const characterIndex = Math.round(Math.random() * characterListLength)
-      password = password + characterList.charAt(characterIndex)
+      const characterIndex = Math.round(Math.random() * characterListLength);
+      password = password + characterList.charAt(characterIndex);
     }
-    return password
-  }
+    return password;
+  };
 
   return (
-    <div className="flex text-center items-center justify-center lg:mt-8 xs:mt-2">
-      <button
-        type="button"
-        className="flex w-max ml-2 px-2 pt-2.5 pb-2 bg-[#ffb800] text-white font-medium font-Poppins text-lg leading-normal rounded-lg shadow-md hover:bg-[#ebaa00] hover:shadow-lg focus:bg-[#ebaa00] focus:shadow-lg focus:outline-none focus:ring-0 active:bg-[#d69b00] active:shadow-lg transition duration-150 ease-in-out align-center"
-        onClick={handleGeneratePassword}
+    <div>
+      <div className="flex text-center items-center justify-center lg:mt-8 xs:mt-2">
+        <button
+          type="button"
+          className="flex w-max ml-2 px-2 pt-2.5 pb-2 bg-[#ffb800] text-white font-medium font-Poppins text-lg leading-normal rounded-lg shadow-md hover:bg-[#ebaa00] hover:shadow-lg focus:bg-[#ebaa00] focus:shadow-lg focus:outline-none focus:ring-0 active:bg-[#d69b00] active:shadow-lg transition duration-150 ease-in-out align-center"
+          onClick={handleGeneratePassword}
         >
-        Generate My Password!
-      </button>
+          Generate My Password!
+        </button>
+      </div>
+      <ToastContainer
+        position="bottom-center"
+        autoClose={1500}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </div>
   );
 }
