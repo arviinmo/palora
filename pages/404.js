b00f27d92ce2447d143af625cd43f8d2576c8f87
Arvin mostafaei <70845998+arviinmo@users.noreply.github.com>
2022-08-02 10:59:09 +0430

    fix 404 and 500 pages

--- pages/404.js
+++ pages/404.js
@@ -4,14 +4,13 @@ export default function Custom404() {
       <h1 className="text-6xl font-extrabold tracking-widest text-black">
         404
       </h1>
-      <h3>Page not found!</h3>
+      <h3 className="font-light font-Poppins">Page not found!</h3>
       <button className="mt-5">
         <a
           href="./"
           className="relative inline-block text-sm font-medium text-[#FF6A3D] group active:text-orange-500 focus:outline-none focus:ring"
         >
           <span className="absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-[#FF6A3D] group-hover:translate-y-0 group-hover:translate-x-0"></span>
-
           <span className="relative block px-8 py-3 bg-[#ffffff] border border-current">
             <router-link>Go Home</router-link>
           </span>
--- pages/500.js
+++ pages/500.js
@@ -1,12 +1,19 @@
 export default function Custom500() {
   return (
     <main className="h-screen w-full flex flex-col justify-center items-center bg-[#ffffff]">
-      <h1 className="font-extrabold tracking-widest text-black text-9xl">
-        500
-      </h1>
-      <div className="bg-[#FF6A3D] font-Poppins text-white px-2 mt-6 text-xl rounded absolute">
-        Server-side error occurred
-      </div>
-    </main>
+    <h1 className="text-6xl font-extrabold tracking-widest text-black">
+      500
+    </h1>
+    <h3>Server-side error occurred!</h3>
+    <a
+      href="mailto:Arvinmostafaeii@gmail.com"
+      className="relative inline-block text-sm font-medium text-[#FF6A3D] group active:text-orange-500 focus:outline-none focus:ring"
+    >
+      <span className="absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-[#FF6A3D] group-hover:translate-y-0 group-hover:translate-x-0"></span>
+      <span className="relative block px-8 py-3 bg-[#ffffff] border border-current">
+        <router-link>author Email: </router-link>
+      </span>
+    </a>
+  </main>
   );
 }
